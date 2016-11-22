a7dc7101e651bef032283f0ce98b1e3539801a8f
mac <sabbirahmed@learniosfreecoms-mac.local>
2016-11-22 15:53:37 +0600

    Initial Commit

---
+++ PickerView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PickerView
+//
+//  Created by Sabbir Ahmed on 11/22/16.
+//  Copyright © 2016 Sabbir Ahmed. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PickerView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PickerView
+//
+//  Created by Sabbir Ahmed on 11/22/16.
+//  Copyright © 2016 Sabbir Ahmed. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
